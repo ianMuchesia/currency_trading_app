19c65c92dae5221fc215221c588ee147451d849b
ianMuchesia <ianmuchesia@yahoo.com>
2022-12-17 16:01:27 +0300

    CHORE calculator

--- components/Pricing.jsx
+++ components/Pricing.jsx
@@ -1,15 +1,81 @@
-import React from "react";
+import React , {useState, useEffect} from "react";
 import styles from "../styles/Home.module.css";
 import LivePricing from "./LivePricing";
-const Pricing = () => {
+const Pricing = ({data}) => {
+  
+  const [currencyDetails , setCurrencyDetails ]  = useState({
+    fromCurrency:"AED",
+    toCurrency:"AED",
+    amount:"1"
+
+  })
+  const [result , setResult] = useState("")
+  const [request , setRequest] = useState(false)
+
+
+  const handleChange = (event)=>{
+    const {value,name } = event.target
+    setCurrencyDetails(prevCurrencyDetails=>{
+      return{
+        ...prevCurrencyDetails,
+        [name]:value
+      }
+    })
+  }
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+    console.log(currencyDetails)
+    setCurrencyDetails(currencyDetails)
+    setRequest(prevRequest=>!prevRequest)
+  }
+  useEffect(()=>{
+    let isMounted =true;
+    const fetchData = async()=>{
+     try{ const myHeaders = new Headers();
+        myHeaders.append("apikey", "g8HhsPzdptDtov298Xz5Q3LBvIsvULYd");
+    
+        const requestOptions = {
+          method: 'GET',
+          redirect: 'follow',
+          headers: myHeaders
+        };
+        const response = await fetch(`https://api.apilayer.com/currency_data/convert?to=${currencyDetails.toCurrency}&from=${currencyDetails.fromCurrency}&amount=${currencyDetails.amount}`, requestOptions)
+        const data = await response.json();
+        console.log(data.result)
+        if(isMounted){
+          setResult(data.result)
+        }
+        
+      }catch(error){
+        console.log(error.message)
+      }
+    }
+    fetchData()
+    return ()=>{isMounted=false}
+  },[request]) 
+
+//Note that in this version, I am using the Object.keys() method to get an array of the object's own enumerable properties, and then using the Array.prototype.map() method to create an array of <option> elements. This approach is more concise and easier to read than using a for...in loop.
+  const currencyList = (list, selectedOption, name ) => (
+    <select className="bg-purple-800 text-white text-lg p-2 rounded-lg m-1 w-[40%] truncate" value={selectedOption} onChange={handleChange} name={`${name}`}>
+      
+      {Object.keys(list).map(prop => (
+
+        <option value={prop} key={prop} >{prop}: {list[prop]}</option>
+      ))}
+    </select>
+  );
+
+
+
   return (
-    <div className="p-4 ">
-      <div
+    <div className="p-4  ">
+      <form
         style={{
           background:
             "linear-gradient(90.07deg, #FF436B -0.79%, rgba(253, 165, 109, 0.98) 99.94%)",
         }}
         className="rounded-[34px] shadow-2xl mx-4 p-3 my-4 sm:p-10"
+        onSubmit={handleSubmit}
       >
         <h2 className="text-white text-5xl font-bold text-center">
           Currency Calculator
@@ -18,29 +84,33 @@ const Pricing = () => {
           There are many variations of passages of Lorem Ipsum form, by injected
           humour randomised words which look .
         </p>
-        <div className="grid gap-6 my-8 sm:grid-cols-2">
+        <div className="grid gap-6 my-8 sm:grid-cols-2 lg:mx-32">
           <div className={styles.glass}>
-            <h5 className="text-2xl m-1  text-gray-200">100</h5>
-            <select className="bg-purple-800 text-white text-lg p-2 rounded-lg m-1">
-              <option className="">EURO</option>
-              <option>BTC</option>
-              <option>USD</option>
-            </select>
+            <input 
+            type='number'
+            value={currencyDetails.amount}
+            placeholder={`${currencyDetails.amount}${"    "}change the amount here`}
+            name='amount'
+            className="bg-transparent w-10 border-none text-gray-200"
+            onChange={handleChange}/>
+            <p>from</p>
+           {currencyList(data, currencyDetails.fromCurrency, "fromCurrency")}
           </div>
           <div className={styles.glass}>
-            <h5 className="text-2xl m-1  text-gray-200">
-              1.215845848
-            </h5>
-            <select className="bg-purple-800 text-white text-lg p-2 rounded-lg m-1">
-              <option className="">BTC</option>
-              <option>USD</option>
-              <option className="">EURO</option>
-            </select>
+            <h5 className="text-2xl m-1  text-gray-200">{result}</h5>
+            {currencyList(data, currencyDetails.toCurrency, "toCurrency")}
           </div>
+          
         </div>
-        <p className="text-xl text-gray-200  mx-4 my-14 text-center">100 EUR = 0.01098913 BTC</p>
-      </div>
-      <LivePricing/>
+        <div className="grid place-items-center">
+            <button className="bg-purple-800 text-white text-lg p-2 rounded-lg m-1">Convert</button>
+          </div>
+          <p className="text-xl text-gray-200  mx-4 my-14 text-center">
+          {currencyDetails.amount}{" "}{currencyDetails.fromCurrency} = {result}{" "}{currencyDetails.toCurrency}
+        </p>
+      </form>
+      
+      <LivePricing />
     </div>
   );
 };
--- components/ServiceCard.jsx
+++ components/ServiceCard.jsx
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Image from "next/image";
 import altcoins from "../public/altcoins.png";
 import bitcoin_calculator from "../public/bitcoin-calculator.png";
 import bitcoin_encryption from "../public/bitcoin-encryption.png";
 import mining_hardware from "../public/mining-hardware.png"
 const ServiceCard = () => {
+
+  useEffect(()=>{
+    const fetchData=async()=>{
+    try{
+      const date = new Date();
+      const currentTime = Math.floor(date.getTime()/100)
+  
+      let oneDay;
+      if(date.getDay()===6){
+        oneDay = currentTime - 2*24*60*60;
+      }else if(date.getDay()===0){
+        oneDay = currentTime -3*24*60*60;
+      }else{
+        oneDay = currentTime - 24*60*60
+      }
+  
+      const oneWeek = currentTime - 7*24*60*60
+      const responses = await fetch(`https://finnhub.io/api/v1/stock/candle?symbol=MSFT&resolution=1&from=${oneWeek}&to=${currentTime}&token=ccq5kriad3i4o9irpul0ccq5kriad3i4o9irpulg
+      `)
+      const data =await  responses.json()
+  
+      console.log(data)
+  
+  
+  
+    }catch(error){
+  
+    }
+  }
+  fetchData()
+  },[])
   return (
     <div>
       {" "}
